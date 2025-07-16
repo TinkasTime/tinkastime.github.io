52fd77aea446f792c6bcc894f115ad6eac3000a5
Tinka <50102291+TinkasTime@users.noreply.github.com>
2025-07-16 14:41:21 +0200

    create Cookie Banner

---
+++ assets/js/cookie-banner-ccm19.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        
+        <script src="https://cloud.ccm19.de/app.js?apiKey=7d653b9e2ca8de85aaf574abd946566a5126c3623128214a&amp;domain=686edcd42266c91a040a54d2" referrerpolicy="origin"></script>
+
+    })(window,document,"script","dataLayer","GTM-XXXXXX");
+</script>
+
